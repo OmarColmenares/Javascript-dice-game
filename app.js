589194c93220b546c5f9b5b22b938a5505f5d163
Omar <omarnazaret@gmail.com>
2020-02-17 10:24:06 -0400

    Start the pig game project

---
+++ app.js
@@ -0,0 +1,34 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, activePlayer, dice, x;
+var scoreZero = document.querySelector('#score-0');
+var scoreOne = document.querySelector('#score-1');
+
+scores = [0 , 0];
+roundScore = 0;
+activePlayer = 1;
+x = document.querySelector('.dice').style.display = 'none';
+
+dice = Math.floor(Math.random() * 6) + 1
+/* Hecho por mi :D
+var y = document.querySelector('#current-' + activePlayer);
+console.log(scoreOne);
+
+
+const roll = document.querySelector('.btn-roll')
+
+roll.addEventListener('click', ()=>{
+    dice = Math.floor(Math.random() * 6) + 1
+    
+    scores[activePlayer] += dice;
+    y.textContent = scores[activePlayer]
+});*/
