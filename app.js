eb65f991efb0d8146e5613399d22e0e93e33881b
Omar <omarnazaret@gmail.com>
2020-02-25 19:53:07 -0400

    Changing routes

--- app.js
+++ app.js
@@ -5,11 +5,11 @@ const app = express()
 //const event = require('events')
 app.use(express.static(path.join(__dirname, 'static')))
 
-app.get('/', (req,res) => {
-  res.sendFile(path.join(__dirname, 'static', 'main.html'));
+app.get('/mode-1', (req,res) => {
+  res.sendFile(path.join(__dirname, 'static', 'mode-1.html'));
 })
 
 app.get('/mode-2', (req,res) => {
-  res.sendFile(path.join(__dirname, 'static', 'mode2 .html'));
+  res.sendFile(path.join(__dirname, 'static', 'mode.2 .html'));
 })
 app.listen(3000);
